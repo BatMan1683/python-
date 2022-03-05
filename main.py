7bdf9f5bdf2d42cd0cf2fb80a65b5ce6bef433ab
haneen <92938917+BatMan1683@users.noreply.github.com>
2022-03-05 00:57:59 +0000

    .

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # this code calculate the number of a string 
- string = input("what is your name : ")
- print("hello " + string )
- print("your name length is : ",len(string))
+ # string = input("what is your name : ")
+ # print("hello " + string )
+ # print("your name length is : ",len(string))
 # qiuck solution 
 # print(len(input("what is tour name? ")))
+# this task will chnage the value for each variable
+a = input("a: ")
+b = input("b: ")
+x = a
+a = b
+b = x
+print("a: " + a)
+print("b: " + b)
+
+
+
+
+
+
