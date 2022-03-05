74562eea18fb56790c45085dbf5960f353dc422a
haneen <92938917+BatMan1683@users.noreply.github.com>
2022-03-05 01:28:12 +0000

    .

--- main.py
+++ main.py
@@ -5,15 +5,18 @@
 # qiuck solution 
 # print(len(input("what is tour name? ")))
 # this task will chnage the value for each variable
-a = input("a: ")
-b = input("b: ")
-x = a
-a = b
-b = x
-print("a: " + a)
-print("b: " + b)
+# a = input("a: ")
+# b = input("b: ")
+# x = a
+# a = b
+# b = x
+# print("a: " + a)
+# print("b: " + b)
 
-
+print("welcome to the Band Name Generator")
+city = input("what's name of the city you Grew up in? : \n ")
+pet = input("what's your pet's name? \n")
+print("your band name could be " + city +" "+ pet )
 
 
 
